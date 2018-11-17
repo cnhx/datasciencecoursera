d85cad33126e6b0f228fac19d8a3361036525b0c
Bryan Hua <me@bryanhua.com>
2018-11-17 17:03:19 -0600

    daily update
    daily update

---
+++ CollectData.R
@@ -0,0 +1,51 @@
+# Load packages
+
+library(tidyverse)
+library(httr)
+library(jsonlite)
+
+#Save appid
+appid <- 570
+
+#Base URL
+url_steam <- 'https://store.steampowered.com/appreviews/'
+
+#Construct API Request
+repos <- GET(url = paste0(url_steam,'/',appid,'?json=1?offset'))
+
+#Exmain response components
+names(repos)
+
+#Check Status, only 200 is fine
+status_code(repos)
+
+#Get Content from Repo
+repo_content <- content(repos)
+
+#repo_df <- lapply(repo_content, function(x){
+#  df <- data.frame(
+#   recommendationID = x$reviews$reviewsrecommendationid,
+#  authorID = x$reviews$author$steamid,
+# language = x$reviews$language,
+#review = x$reviews$review
+#)
+#})
+
+dfrepo <- as.data.frame(repo_content)
+repo_reviews <- repo_content$reviews
+repo_reviews <-as.data.frame(repo_reviews)
+
+dfsteamrepo <-list()
+while(TRUE){
+        num = 20
+        steamrepo <-repos <- GET(url = paste0(url_steam,'/',appid,'?json=1?offset=',num))
+        print("Offset now is ", num)
+        if(status_code(steamrepo)==200){
+                steamrepo_content<-content(steamrepo)
+                dfsteamrepo <- c(dfsteamrepo,steamrepo_content$reviews)
+                num <- num + 20
+        }else{
+                break
+        }
+}
+
